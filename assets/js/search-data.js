dcfc354e7eb302fbff4e9a750c790ee4136ce777
songlkkevin <95568893+songlkkevin@users.noreply.github.com>
2025-06-07 02:32:11 +0000

    Deploying to gh-pages from @ GAsys-web/GAsys-web@5da45889f59579527ac710c4ddb3f0bb987a1814 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,55 +7,55 @@ ninja.data = [{
     title: "about",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/GAsys-website/";
+      window.location.href = "/";
     },
   },{id: "dropdown-program-committee",
               title: "program committee",
               description: "",
               section: "Dropdown",
               handler: () => {
-                window.location.href = "/GAsys-website/committee/program";
+                window.location.href = "/committee/program";
               },
             },{id: "dropdown-organizing-committee",
               title: "organizing committee",
               description: "",
               section: "Dropdown",
               handler: () => {
-                window.location.href = "/GAsys-website/committee/organizing";
+                window.location.href = "/committee/organizing";
               },
             },{id: "dropdown-steering-committee",
               title: "steering committee",
               description: "",
               section: "Dropdown",
               handler: () => {
-                window.location.href = "/GAsys-website/committee/steering";
+                window.location.href = "/committee/steering";
               },
             },{id: "nav-program",
           title: "program",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/GAsys-website/program/";
+            window.location.href = "/program/";
           },
         },{id: "dropdown-papers",
               title: "papers",
               description: "",
               section: "Dropdown",
               handler: () => {
-                window.location.href = "/GAsys-website/cfp/";
+                window.location.href = "/cfp/";
               },
             },{id: "dropdown-grand-challenges",
               title: "grand challenges",
               description: "",
               section: "Dropdown",
               handler: () => {
-                window.location.href = "/GAsys-website/cfg/";
+                window.location.href = "/cfg/";
               },
             },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/GAsys-website/books/the_godfather/";
+              window.location.href = "/books/the_godfather/";
             },},{id: "news-the-website-is-online",
           title: 'The website is online.',
           description: "",
@@ -63,47 +63,47 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/GAsys-website/projects/1_project/";
+              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/GAsys-website/projects/2_project/";
+              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/GAsys-website/projects/3_project/";
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/GAsys-website/projects/4_project/";
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/GAsys-website/projects/5_project/";
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/GAsys-website/projects/6_project/";
+              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/GAsys-website/projects/7_project/";
+              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/GAsys-website/projects/8_project/";
+              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/GAsys-website/projects/9_project/";
+              window.location.href = "/projects/9_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
